ab920d1b014029eb3f86d2417ee44965429e0ad4
AndyM <andymreyes@outlook.es>
2021-09-08 17:24:54 -0400

    Galeria -> Overlay completado

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const grid = new Muuri('.grid', {
     }
 });
 
+//Agregamos los listener de los enlaces para filtrar por categorias.
 window.addEventListener('load', () => {
     grid.refreshItems().layout();
     document.getElementById('grid').classList.add('imagenes-cargadas');
@@ -23,5 +24,36 @@ window.addEventListener('load', () => {
                 grid.filter(`[data-categoria= ${categoria}]`);
             }
         });
+    });
+
+    //Agregamos el listener para la barra de busqueda.
+    document.querySelector('#barra-busqueda').addEventListener('input', (evento) => {
+        const busqueda = evento.target.value;
+        grid.filter((item) => item.getElement().dataset.etiquetas.includes(busqueda));
+    });
+    //Agregamos listener para las imagenes
+    const overlay = document.getElementById('overlay');
+    document.querySelectorAll('.grid .item img').forEach((elemento) => {
+
+        elemento.addEventListener('click', () => {
+            const ruta = elemento.getAttribute('src');
+            const descripcion = elemento.parentNode.parentNode.dataset.descripcion;
+
+            overlay.classList.add('active');
+            document.querySelector('#overlay img').src = ruta;
+            document.querySelector('#overlay .descripcion').innerHTML = descripcion;
+        });
+    });
+
+    //EventListener del boton de cerrar
+    document.querySelector('#btn-cerrar-popup').addEventListener('click', () => {
+        overlay.classList.remove('active');
+    });
+
+    //EventListener del boton de cerrar
+    overlay.addEventListener('click', (evento) => {
+        if (evento.target.id === 'overlay') {
+            overlay.classList.remove('active');
+        }
     });
 });
